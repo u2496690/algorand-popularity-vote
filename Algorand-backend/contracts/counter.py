1a633d0d0510eac17110c8c8920c4143ca92a022
Lucian Duta <51519715+lucian-duta@users.noreply.github.com>
2023-03-20 23:22:10 +0000

    backend

---
+++ Algorand-backend/contracts/counter.py
@@ -0,0 +1,69 @@
+from pyteal import *
+import os
+
+"""Basic Counter Application"""
+
+
+def approval_program():
+    handle_creation = Seq(
+        App.globalPut(Bytes("Count"), Int(0)),
+        Return(Int(1))
+    )
+    # handle creation function above
+    handle_optin = Return(Int(0))
+
+    handle_closeout = Return(Int(0))
+
+    handle_updateapp = Return(Int(0))
+
+    handle_deleteapp = Return(Int(0))
+    # conditional statement below
+    # delete app above
+
+    scratchCount = ScratchVar(TealType.uint64)
+    add = Seq([
+        scratchCount.store(App.globalGet(Bytes("Count"))),
+        App.globalPut(Bytes("Count"), scratchCount.load() + Int(1)),
+        Return(Int(1))
+    ])
+
+    deduct = Seq([
+        scratchCount.store(App.globalGet(Bytes("Count"))),
+        If(scratchCount.load() > Int(0),
+           App.globalPut(Bytes("Count"), scratchCount.load() - Int(1)),
+           ),
+        Return(Int(1))
+    ])
+
+    handle_noop = Seq(
+        # First, fails immediately if this transaction is grouped with others
+        Assert(Global.group_size() == Int(1)),
+        Cond(
+            [Txn.application_args[0] == Bytes("Add"), add],
+            [Txn.application_args[0] == Bytes("Deduct"), deduct]
+        )
+    )
+# conditional below
+    program = Cond(
+        [Txn.application_id() == Int(0), handle_creation],
+        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
+        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
+        [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
+        [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
+        [Txn.on_completion() == OnComplete.NoOp, handle_noop]
+    )
+    return compileTeal(program, Mode.Application, version=5)
+
+
+def clear_state_program():
+    program = Return(Int(1))
+    return compileTeal(program, Mode.Application, version=5)
+
+
+if __name__ == "__main__":
+    path = "./contracts/artifacts"
+    with open(os.path.join(path, "counter_approval.teal"), 'w') as f:
+        f.write(approval_program())
+
+    with open(os.path.join(path, "counter_clear.teal"), 'w') as f:
+        f.write(clear_state_program())
---
+++ Algorand-backend/scripts/2-first-transaction.js
@@ -0,0 +1,60 @@
+import dotenv from "dotenv";
+import algosdk from "algosdk";
+dotenv.config();
+let myaccount = algosdk.mnemonicToSecretKey(
+  "into dove immense rich melt isolate amount replace outdoor act ship alert sight stand abstract arrest circle ridge impose insane remind camera camera absorb fall"
+);
+
+const baseServer = "https://testnet-algorand.api.purestake.io/ps2";
+const port = "";
+const token = {
+  "X-API-Key": "QSiMVqlcHn1uMl22I9MXK3bKrPlQLRD64A1Ux9hZ",
+};
+
+const algodClient = new algosdk.Algodv2(token, baseServer, port);
+// This variable is our client. It is the link between our code and the blockchain
+
+(async () => {
+  try {
+    let params = await algodClient.getTransactionParams().do();
+
+    // reciever will be some random address.
+    const receiver =
+      "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
+    const enc = new TextEncoder();
+    const note = enc.encode("My first transaction on Algo!");
+    let amount = 100000; // equals .1 ALGO
+    let sender = myaccount.addr;
+
+    let txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: sender,
+      to: receiver,
+      amount: amount,
+      node: note,
+      suggestedParams: params,
+    });
+
+    console.log("HERE3");
+
+    let accountInfo = await algodClient.accountInformation(myaccount.addr).do();
+    console.log("Account balance: %d microAlgos", accountInfo.amount);
+
+    // sign transaction
+
+    let signedTxn = txn.signTxn(myaccount.sk);
+    let txId = txn.txID().toString();
+    console.log("Signed transaction with txID: %s", txId);
+
+    await algodClient.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
+    accountInfo = await algodClient.accountInformation(myaccount.addr).do();
+    console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);
+    console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
+    console.log("Account balance: %d microAlgos", accountInfo.amount);
+  } catch (err) {
+    console.error("Failed to get apps from the sdk", err);
+    process.exit(1);
+  }
+})();
